f996f193403c03c6b85f34ee10c747d1b1c67c7b
Arter Tendean <arter.tendean.07@gmail.com>
2021-05-25 19:35:13 +0800

    adding InfiniteScroll

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,55 @@
-import Image from "next/image";
 import Masonry from "react-masonry-css";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useState } from "react";
 
 import { Navbar } from "../components/molecules";
 
 export default function Home(props) {
+  const [images, setImages] = useState(props.images);
+  const [hasMore, setHasMore] = useState(true);
+  const [pageNow, setPageNow] = useState(2);
+
+  const fetchMoreData = () => {
+    fetch(`https://api.animemoe.us/images/?page=${pageNow}`)
+      .then((res) => res.json())
+      .then((response) => {
+        if (response.next === null) {
+          setHasMore(false);
+        }
+
+        setImages(images.concat(response.results));
+        setPageNow(parseInt(pageNow) + 1);
+      });
+  };
+
   return (
     <div>
       <Navbar />
 
-      <div className="container-fluid mt-3">
-        <Masonry
-          breakpointCols={{ default: 5, 1100: 4, 700: 3, 500: 2 }}
-          className="my-masonry-grid"
-          columnClassName="my-masonry-grid_column"
-        >
-          {props.images.results.map((image) => (
-            <div
-              key={image.id}
-              className="card  border-0 rounded-0"
-              style={{ width: "100%" }}
-            >
-              <img src={image.thumbnail} className=" shadow-sm" />
-            </div>
-          ))}
-        </Masonry>
-      </div>
+      <InfiniteScroll
+        dataLength={images.length}
+        next={fetchMoreData}
+        hasMore={hasMore}
+        loader={<h4 className="text-center">Loading...</h4>}
+      >
+        <div className="container-fluid mt-3">
+          <Masonry
+            breakpointCols={{ default: 5, 1100: 4, 700: 3, 500: 2 }}
+            className="my-masonry-grid"
+            columnClassName="my-masonry-grid_column"
+          >
+            {images.map((image) => (
+              <div
+                key={image.id}
+                className="card  border-0 rounded-0"
+                style={{ width: "100%" }}
+              >
+                <img src={image.thumbnail} className=" shadow-sm" />
+              </div>
+            ))}
+          </Masonry>
+        </div>
+      </InfiniteScroll>
     </div>
   );
 }
@@ -33,5 +58,5 @@ export async function getStaticProps() {
   const res = await fetch("https://api.animemoe.us/images/");
   const response = await res.json();
 
-  return { props: { images: response } };
+  return { props: { images: response.results } };
 }
