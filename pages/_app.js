e421c20632ea65daabed498d9f8ae923b62a1047
Arter Tendean <arter.tendean.07@gmail.com>
2021-05-25 13:52:11 +0800

    adding bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
-
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
+        crossOrigin="anonymous"
+      ></link>
+      <Component {...pageProps} />;
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+        crossOrigin="anonymous"
+      ></script>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
