6b4dcbdc2791af655358e448c1893a5c466c48dd
Arter Tendean <arter.tendean.07@gmail.com>
2021-06-27 20:16:06 +0800

    update image detail

--- pages/[id].js
+++ pages/[id].js
@@ -15,53 +15,66 @@ export default function Detail(props) {
       description={`Waifu ${props.response.image_id} by ${props.response.creator_name}`}
       keywords={`${props.response.creator_name}, ${props.response.creator_username}, ${props.response.image_id}, animemoeus, waifu animemoeus`}
     >
-      <div style={{ minHeight: "100vh" }}>
+      <div className="min-h-screen">
         <Navbar />
 
-        <div className="container-sm p-1 mt-3">
-          <div className="container-fluid border rounded shadow bg-white mb-3">
-            <div className="row">
-              <div className="col-lg-7 p-1">
+        <div className="container mx-auto p-1 pt-3 mb-5 xl:px-32">
+          {/* grid */}
+          <div className="grid grid-cols-1 lg:grid-cols-3 p-1 border rounded shadow-lg">
+            {/* image */}
+            <div className="col-span-1 p-0 lg:col-span-2 p-1">
+              <div className="border rounded shadow-md p-0 bg-gray-300 overflow-hidden">
                 <Image
                   src={props.response.original_image}
                   width={props.response.width}
                   height={props.response.height}
-                  quality={100}
-                  alt={`Image ${props.response.image_id} by ${props.response.creator_name}`}
-                  className="rounded  border p-0"
+                  layout="responsive"
+                  alt="arter"
                 />
               </div>
-              <div className="col-lg-5 pt-2">
-                <p className="text-center fs-4 m-1">
+            </div>
+            {/* end image */}
+            {/* image info */}
+            <div className="col-span-1 p-0 pt-1 lg:p-1">
+              <div className="">
+                <h2 className="text-center text-xl font-semibold">
                   {props.response.image_id}
-                </p>
-                <hr className="m-1" />
-                {props.response.creator_name && (
-                  <p>Creator: {props.response.creator_name}</p>
-                )}
-
-                {props.response.caption && <p>{props.response.caption}</p>}
-
-                {props.response.source && (
-                  <p>
-                    Source:{" "}
-                    <a
-                      href={`${props.response.source}`}
-                      className="text-decoration-none"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      {props.response.source}
-                    </a>
-                  </p>
-                )}
+                </h2>
+                <div className="pl-2">
+                  <hr className="m-1" />
+                  {props.response.creator_name && (
+                    <p>Creator: {props.response.creator_name}</p>
+                  )}
+                  <br />
+                  {props.response.caption && <p>{props.response.caption}</p>}
+                  <br />
+                  {props.response.source && (
+                    <p>
+                      Source:{" "}
+                      <a
+                        href={props.response.source}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="text-blue-700"
+                      >
+                        {props.response.source}
+                      </a>
+                    </p>
+                  )}
+                </div>
               </div>
             </div>
-            <hr />
-            <div>
-              <Disqus data={props.response} />
+            {/* end image info */}
+            {/* disqus */}
+            <div className="col-span-1 p-0 lg:col-span-3 p-1">
+              <hr className="my-3" />
+              <div className="container px-2">
+                <Disqus data={props.response} />
+              </div>
             </div>
+            {/* end disqus */}
           </div>
+          {/* end grid */}
         </div>
       </div>
     </Layout>
