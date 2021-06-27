ec1dbb37ed554cc723752d035e3bd5b1c18f0bc4
Arter Tendean <arter.tendean.07@gmail.com>
2021-06-27 16:16:46 +0800

    update navbar

--- components/molecules/Navbar/index.js
+++ components/molecules/Navbar/index.js
@@ -2,48 +2,36 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light shadow-sm">
-      <div className="container-fluid">
-        <Link href="/">
-          <a className="navbar-brand">Waifu Collections</a>
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <a
-                className="nav-link active"
-                aria-current="page"
-                href="https://t.me/animemoeus_bot"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <button type="button btn-sm" className="btn btn-outline-dark">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-telegram"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z" />
-                  </svg>{" "}
-                  Add Waifu
-                </button>
-              </a>
-            </li>
-          </ul>
+    <nav>
+      <div className="flex justify-between items-center h-14 bg-white shadow">
+        <div className="px-3">
+          <Link href="/">
+            <a>
+              <span className="text-xl">Waifu Collections</span>
+            </a>
+          </Link>
+        </div>
+        <div className="px-4">
+          <a
+            href="https://t.me/animemoeus_bot/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z"
+              />
+            </svg>
+          </a>
         </div>
       </div>
     </nav>
--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,9 @@ import Script from "next/script";
 import NProgress from "nprogress";
 import { useState } from "react";
 
-import "../styles/custom.css";
 import "animate.css/animate.min.css";
+import "tailwindcss/tailwind.css";
+import "../styles/custom.css";
 import "../styles/nprogress.css";
 
 // nprogress loading
@@ -17,27 +18,13 @@ function MyApp({ Component, pageProps }) {
   const [pageNow, setPageNow] = useState(1);
 
   return (
-    <>
-      <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
-        crossOrigin="anonymous"
-      />
-      <Script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
-        crossOrigin="anonymous"
-      />
-
-      <Component
-        {...pageProps}
-        images={images}
-        setImages={setImages}
-        pageNow={pageNow}
-        setPageNow={setPageNow}
-      />
-    </>
+    <Component
+      {...pageProps}
+      images={images}
+      setImages={setImages}
+      pageNow={pageNow}
+      setPageNow={setPageNow}
+    />
   );
 }
 
