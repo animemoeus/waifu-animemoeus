168a9e5078e8b78cd9fad2e4450ba5eeabc60388
Arter Tendean <arter.tendean.07@gmail.com>
2024-05-02 01:42:11 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,12 +4,7 @@ module.exports = {
       "64.media.tumblr.com",
       "cdn.discordapp.com",
       "media.discordapp.net",
-<<<<<<< HEAD
       "api.animemoe.us",
-=======
-      "cdn.discordapp.xyz",
-      "media.discordapp.xyz"
->>>>>>> 8ec467d43990eeddc06816894d53ac7f6bb69a04
     ],
   },
 };
