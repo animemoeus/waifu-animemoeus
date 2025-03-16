6a1e96b60f9289c9105f29a39004c62774afe636
Arter Tendean <arter.tendean.07@gmail.com>
2025-03-16 19:51:04 +0700

    feat: add ArterTendean page and simplify Home component

---
+++ src/app/artertendean/page.tsx
@@ -0,0 +1,7 @@
+export default function ArterTendean(){
+    return(
+        <>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum exercitationem magnam quia illum officia praesentium impedit dignissimos architecto, corrupti possimus nisi laborum, voluptas quaerat optio consequuntur rerum vero corporis blanditiis!</p>
+        </>
+    )
+}
