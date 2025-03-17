53eb7ccd391f009a01579920ed0bdff7ae1ff094
Arter Tendean <arter.tendean.07@gmail.com>
2025-03-17 23:44:23 +0700

    Clean the Code (#364)
    * chore: add Prettier for code formatting and update package dependencies
    
    * fix: update metadata title and description in layout.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
 import ImageCard from "@/components/ui/image-card";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
-import Masonry from 'react-masonry-css'
+import Masonry from "react-masonry-css";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 export interface IWaifu {
@@ -32,16 +32,23 @@ interface IWaifuResponse {
 
 export default function Home() {
   const [waifus, setWaifus] = useState<IWaifu[]>([]);
-  const [nextUrl, setNextUrl] = useState<string | null>("https://api.animemoe.us/waifu/");
+  const [nextUrl, setNextUrl] = useState<string | null>(
+    "https://api.animemoe.us/waifu/"
+  );
   const [hasMore, setHasMore] = useState(true);
 
   const queryClient = useQueryClient();
 
   // Initial data fetch
-  const { isLoading: isLoadingWaifuResponse, isError: isErrorWaifuResponse, error: errorWaifuResponse } = useQuery<IWaifuResponse>({
+  const {
+    isLoading: isLoadingWaifuResponse,
+    isError: isErrorWaifuResponse,
+    error: errorWaifuResponse,
+  } = useQuery<IWaifuResponse>({
     queryKey: ["home"],
     queryFn: async () => {
-      if (!nextUrl) return { count: 0, next: null, previous: null, results: [] };
+      if (!nextUrl)
+        return { count: 0, next: null, previous: null, results: [] };
 
       const response = await fetch(nextUrl);
       if (!response.ok) {
@@ -86,32 +93,18 @@ export default function Home() {
     }
   };
 
-  useEffect(() => {
-    console.log('waifus', waifus);
-  }, [waifus]);
-
-  useEffect(() => {
-    console.log('next url', nextUrl);
-  }, [nextUrl]);
-
-  useEffect(() => {
-    console.log('is loading', isLoadingWaifuResponse);
-  }, [isLoadingWaifuResponse]);
-
-  useEffect(() => {
-    console.log('error waifu', isErrorWaifuResponse);
-  }, [isErrorWaifuResponse]);
-
-  useEffect(() => {
-    console.log('error', errorWaifuResponse);
-  }, [errorWaifuResponse]);
-
   return (
     <div>
-      {isLoadingWaifuResponse && waifus.length === 0 && <p className="text-center">Loading...</p>}
-      {isErrorWaifuResponse && <p className="text-center text-red-500">Error loading data</p>}
+      {isLoadingWaifuResponse && waifus.length === 0 && (
+        <p className="text-center">Loading...</p>
+      )}
+      {isErrorWaifuResponse && (
+        <p className="text-center text-red-500">Error loading data</p>
+      )}
 
-      <h1 className="text-center mt-2 mb-3 text-2xl md:text-3xl lg:text-4xl font-bold overflow-auto">instagram.com/arter_tendean</h1>
+      <h1 className="text-center mt-2 mb-3 text-2xl md:text-3xl lg:text-4xl font-bold overflow-auto">
+        instagram.com/arter_tendean
+      </h1>
 
       <InfiniteScroll
         dataLength={waifus.length}
@@ -123,14 +116,22 @@ export default function Home() {
         <Masonry
           breakpointCols={{ default: 5, 1100: 4, 700: 3, 500: 2 }}
           className="my-masonry-grid pl-1 pr-2"
-          columnClassName="my-masonry-grid_column">
+          columnClassName="my-masonry-grid_column"
+        >
           {waifus.map((waifu) => (
             <div key={waifu.id} className="rounded-base">
-              <ImageCard imageUrl={waifu.thumbnail} caption={waifu.caption} width={waifu.width} height={waifu.height} blurDataURL={waifu.blur_data_url} raw={waifu} />
+              <ImageCard
+                imageUrl={waifu.thumbnail}
+                caption={waifu.caption}
+                width={waifu.width}
+                height={waifu.height}
+                blurDataURL={waifu.blur_data_url}
+                raw={waifu}
+              />
             </div>
           ))}
         </Masonry>
       </InfiniteScroll>
     </div>
-  )
+  );
 }
